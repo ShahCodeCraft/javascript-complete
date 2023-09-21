9d3ecda0db9015a4ef4e317a6cb7be521d6b3ea9
ShahCodeCraft <ershahalam22@gmail.com>
2023-09-21 23:23:24 +0530

    variable add

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14455
+let accountEmail = "shahalam12@gmail.com"
+var accountPassword = "1234"
+accountCity = "Muzaffarpur"
+
+// const accountId = 14  //not allow
+// let accountEmail = "alam12@gmail.com"
+// var accountPassword = "1234"
+// accountCity = "Muzaffarpur"
+
+console.log(accountId)
