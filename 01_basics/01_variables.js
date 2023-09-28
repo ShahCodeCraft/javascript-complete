76617e1b4dba232d78e0a6439acb7f9147e9b02c
ShahCodeCraft <ershahalam22@gmail.com>
2023-09-28 10:38:59 +0530

    conversion of datatype

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,35 @@
-const accountId = 14455
-let accountEmail = "shahalam12@gmail.com"
-var accountPassword = "1234"
-accountCity = "Muzaffarpur"
+// const accountId = 14455
+// let accountEmail = "shahalam12@gmail.com"
+// var accountPassword = "1234"
+// accountCity = "Muzaffarpur"
 
 // const accountId = 14  //not allow
 // let accountEmail = "alam12@gmail.com"
 // var accountPassword = "1234"
 // accountCity = "Muzaffarpur"
 
-console.log(accountId)
+console.log(accountId);
+
+ //Code and Chai
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
