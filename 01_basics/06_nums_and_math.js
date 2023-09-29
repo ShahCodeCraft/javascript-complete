8dc9da9e4457b2bae25cfabcaf09c371767c4c9d
ShahCodeCraft <ershahalam22@gmail.com>
2023-09-29 11:36:19 +0530

    Math and Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+// const score = 400;
+// console.log(score,typeof score);
+
+// const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2),typeof balance);
+
+// let num = 15.56789;
+// let newNum = num.toFixed(10);
+// console.log(newNum);
+
+// let num = 15.56789;
+// console.log(num.toPrecision(3)); 
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// let num1 = Number.MAX_VALUE;
+// let num2 = Number.MIN_VALUE;
+// let num3 = Number.MAX_SAFE_INTEGER;
+// console.log(num1)
+
+// +++++++++++++++++++ Maths +++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-22));    // convert in positive 4
+// console.log(Math.round(6.89)); 
+// console.log(Math.ceil(6.0));     // select Top value
+// console.log(Math.floor(6.66));   // select Bottom value
+// console.log(Math.max(5,8,3,5));
+// console.log(Math.min(6,4,7,3,6));
+
+// console.log(Math.random())
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.random() * (max - min + 1)) 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
