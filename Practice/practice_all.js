4246fa7e4433abc9ad11c8869cfb3a1ed18b0497
ShahCodeCraft <ershahalam22@gmail.com>
2024-02-10 13:25:09 +0530

    Add Practice section

---
+++ Practice/practice_all.js
@@ -0,0 +1,61 @@
+let name = "Shah Alam";
+name = "Shahbaz Alam"
+console.log("Name : "+name)
+// name = "Shahbaz Alam"
+
+var age = 25;
+age = 30;
+console.log("Age : "+age)
+// age = 30;
+
+const gen = "Male";
+// gen = "Female";
+console.log("Gender :"+gen)
+// gen = "Female"
+
+let num = 39;
+let str = "Shah";
+let str1 = "";
+let bool = false;
+let nul = null;
+let unde = undefined;
+let obj = {
+  
+}
+let arr = [];
+console.log(typeof num ,typeof str, typeof str1, typeof bool,typeof nul, typeof unde, typeof obj,typeof arr)
+
+ let score = "Shahnawaz";
+ console.log(typeof score);
+ console.log(typeof(score));
+ 
+ let a = true;
+ console.log(a +":"+typeof a)
+ 
+ let b = Number(a);
+ console.log(b+":"+typeof b)
+ 
+ let numstr = "333";
+ console.log(numstr + ":" + typeof numstr)
+ 
+ let numbstr = Number(numstr);
+ console.log(numbstr + ":" + typeof numbstr)
+ 
+ let strnum = "33abc";
+ console.log(strnum + ":" +typeof strnum);
+ 
+ let strnumb = Number(strnum);
+ console.log(strnumb)
+ 
+ let isloggedin = "Shah";
+ console.log(isloggedin + ":" + typeof isloggedin)
+ let booleanloggedin = Boolean(isloggedin);
+ console.log(booleanloggedin + ":" + typeof booleanloggedin)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
